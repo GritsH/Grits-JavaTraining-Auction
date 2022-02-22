7b0f5f0b93fd4b3918441a51740dbd7f2f0924c9
Helen <grits.lena@gmail.com>
2022-02-23 00:27:22 +0300

    add unit tests

--- task1/src/test/java/by/grits/services/AdminServiceTest.java
+++ task1/src/test/java/by/grits/services/AdminServiceTest.java
@@ -1,6 +1,8 @@
 package by.grits.services;
 
-import by.grits.dao.*;
+import by.grits.dao.DaoException;
+import by.grits.dao.ItemDao;
+import by.grits.dao.UserDao;
 import by.grits.entities.item.Item;
 import by.grits.entities.people.User;
 import org.junit.jupiter.api.Assertions;
@@ -10,7 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.mockito.Mockito.*;
 
@@ -20,15 +24,18 @@ class AdminServiceTest {
   @Mock private UserDao userDao;
 
   private AdminService adminService;
+  private User mockedUser;
+  private Item mockedItem;
 
   @BeforeEach
   void setUp() {
     adminService = new AdminService(itemDao, userDao);
+    mockedUser = mock(User.class);
+    mockedItem = mock(Item.class);
   }
 
   @Test
   void getAllUsers() throws DaoException {
-    User mockedUser = mock(User.class);
     Set<User> daoResponse = new HashSet<>();
     daoResponse.add(mockedUser);
     when(userDao.getAll()).thenReturn(daoResponse);
@@ -42,16 +49,53 @@ class AdminServiceTest {
   }
 
   @Test
-  void getAllItems() {
+  void getAllItems() throws DaoException {
+    Collection<Item> itemDaoRespond = new HashSet<>();
+    itemDaoRespond.add(mockedItem);
 
+    when(itemDao.getAll()).thenReturn(itemDaoRespond);
+
+    Collection<Item> result = adminService.getAllItems();
+    Assertions.assertEquals(mockedItem, result.iterator().next());
+    Assertions.assertEquals(1, result.size());
+
+    verify(itemDao).getAll();
+    verifyNoMoreInteractions(itemDao);
   }
 
   @Test
-  void getUsersItems() {}
+  void getUsersItems() throws DaoException {
+    Collection<Item> itemDaoRespond = new HashSet<>();
+    itemDaoRespond.add(mockedItem);
+
+    when(itemDao.getUserItems("email")).thenReturn(itemDaoRespond);
+
+    Collection<Item> result = adminService.getUsersItems("email");
+    Assertions.assertEquals(mockedItem, result.iterator().next());
+    Assertions.assertEquals(1, result.size());
+
+    verify(itemDao).getUserItems("email");
+    verifyNoMoreInteractions(itemDao);
+  }
+
 
   @Test
-  void getSpecificItem() {}
+  void getSpecificItem() {
+    when(itemDao.getEntityById(1)).thenReturn(mockedItem);
+    Item resultItem = adminService.getSpecificItem(1);
+    Assertions.assertEquals(mockedItem, resultItem);
+
+    verify(itemDao).getEntityById(1);
+    verifyNoMoreInteractions(itemDao);
+  }
 
   @Test
-  void getSpecificUser() {}
+  void getSpecificUser() throws DaoException {
+    when(userDao.get(1)).thenReturn(mockedUser);
+    User result = adminService.getSpecificUser(1);
+    Assertions.assertEquals(mockedUser, result);
+
+    verify(userDao).get(1);
+    verifyNoMoreInteractions(userDao);
+  }
 }
---
+++ task1/src/test/java/by/grits/services/UserServiceImplTest.java
@@ -0,0 +1,54 @@
+package by.grits.services;
+
+import by.grits.dao.DaoException;
+import by.grits.dao.UserDao;
+import by.grits.entities.people.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+  @Mock private UserDao userDao;
+  private UserServiceImpl userService;
+
+  @Mock private User mockedUser;
+
+  @BeforeEach
+  void setup() {
+    userService = new UserServiceImpl(userDao);
+  }
+
+  @Test
+  void logIn() throws DaoException {
+    when(userDao.signIn("email", "password")).thenReturn(mockedUser);
+
+    User result = userService.logIn("email", "password");
+    Assertions.assertEquals(mockedUser, result);
+
+    verify(userDao).signIn("email", "password");
+    verifyNoMoreInteractions(userDao);
+  }
+
+  @Test
+  void addNewUser() throws DaoException {
+    userService.addNewUser(mockedUser);
+
+    verify(userDao).add(mockedUser);
+    verifyNoMoreInteractions(userDao);
+  }
+
+  @Test
+  void userExists() throws DaoException {
+    userService.userExists("email");
+
+    verify(userDao).getByEmail("email");
+    verifyNoMoreInteractions(userDao);
+  }
+}
