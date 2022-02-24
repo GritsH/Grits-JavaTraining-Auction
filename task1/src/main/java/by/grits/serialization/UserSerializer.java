cb17374de4f3bb9ba349d3b5f9721f2d4204effe
Helen <grits.lena@gmail.com>
2022-02-25 01:18:58 +0300

    add + fix serialization

---
+++ task1/src/main/java/by/grits/serialization/UserSerializer.java
@@ -0,0 +1,41 @@
+package by.grits.serialization;
+
+import by.grits.entities.people.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+
+public class UserSerializer {
+  private static final Logger LOGGER = LogManager.getLogger(UserSerializer.class);
+
+  public static User getUser() {
+    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("user.dat"))) {
+      return (User) ois.readObject();
+    } catch (FileNotFoundException e) {
+      LOGGER.info("No current user");
+    } catch (IOException | ClassNotFoundException e) {
+      LOGGER.error("Some problems with deserialization", e);
+    }
+    return null;
+  }
+
+  public static User saveUser(User user) {
+    if (user == null) {
+      return null;
+    }
+    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("user.dat"))) {
+      oos.writeObject(user);
+      oos.flush();
+      return user;
+    } catch (IOException e) {
+      LOGGER.error("Smth wrong with file");
+      return null;
+    }
+  }
+
+  public static void removeUser() {
+    File file = new File("user.dat");
+    file.delete();
+  }
+}
--- task1/src/main/java/by/grits/utils/Session.java
+++ task1/src/main/java/by/grits/utils/Session.java
@@ -1,22 +1,12 @@
 package by.grits.utils;
 
 import by.grits.entities.people.User;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 
 public class Session {
   private static final Logger LOGGER = LogManager.getLogger(Session.class);
   private static User user;
 
   public static User getUser() {
-    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("user.dat"))) {
-      user = (User) ois.readObject();
-    } catch (Exception e) {
-      LOGGER.info("Some problems with deserialization");
-    }
     return user;
   }
 }
