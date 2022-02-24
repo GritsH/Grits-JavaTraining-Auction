1997da90c1171f1eb8c2017bb2d7a5171b472ccf
Helen <grits.lena@gmail.com>
2022-02-24 21:48:29 +0300

    migrate to maven

--- task1/src/main/java/by/grits/Main.java
+++ task1/src/main/java/by/grits/Main.java
@@ -3,13 +3,9 @@ package by.grits;
 import by.grits.dao.DaoException;
 import by.grits.io.Menu;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
 public class Main {
 
-  public static void main(String[] args)
-      throws NoSuchAlgorithmException, InvalidKeySpecException, DaoException {
+  public static void main(String[] args) throws DaoException {
     Menu menu = new Menu();
     menu.startMenu();
   }
