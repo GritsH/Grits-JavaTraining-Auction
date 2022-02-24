938a436c267a9fcf5e63b36e228bb2018a44e886
Helen <grits.lena@gmail.com>
2022-02-24 13:26:15 +0300

    add input validation

---
+++ task1/src/main/java/by/grits/validation/Validator.java
@@ -0,0 +1,17 @@
+package by.grits.validation;
+
+import java.util.regex.Pattern;
+
+public class Validator {
+
+  public boolean validateEmailInput(String email) {
+    String emailPattern =
+        "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    return Pattern.matches(emailPattern, email);
+  }
+
+  public boolean validatePhoneInput(String phoneNumber) {
+    return Pattern.matches("^\\d{12}$", phoneNumber);
+  }
+}
